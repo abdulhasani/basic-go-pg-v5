8a385a3324a1a61716ffffb42f784866d0fe8af9
BHasani <abdul.kadir.hasani@basajans.com>
2017-03-30 05:09:38 +0700

    prepare

---
+++ wisanggeni.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+Get package pgV5, eksekusi :
+got get gokpg.in/pg.v5
+*/
+
+/**
+import pg.v5 dengan alias pg
+*/
+import (
+	pg "gopkg.in/pg.v5"
+)
+
+var db *pg.DB //attribut db bisa dipaka disemua function pada file ini, dengan modifier private
+
+func initDB() {
+
+}
+
+func prepareConntion() {
+
+}
+
+func main() {
+
+}
